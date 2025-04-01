c9038afb94231fd862781a8d58b05a5aac507ac2
Akhil Vuputuri <e0309566@u.nus.edu>
2025-04-02 01:02:57 +0800

    Add autocomplete

--- client/src/app/recommend/page.tsx
+++ client/src/app/recommend/page.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect, FormEvent } from "react";
 import { Button } from "@/components/ui/button"
 import { Input } from "@/components/ui/input"
 import BookCard from "../components/bookCard";
+import { Autocomplete } from "../components/autocomplete";
 
 type Book = {
   title: string;
@@ -12,11 +13,48 @@ type Book = {
 }
 
 
+
 export default function RecommendBook() {
   const [searchTitle, setSearchTitle] = useState("");
+  const [selectedTitle, setSelectedTitle] = useState("");
   const [isLoading, setIsLoading] = useState<boolean>(false)
   const [recommendedBooks, setRecommendedBooks] = useState<Book[]>([]);
 
+  const [allBookTitles, setAllBookTitles] = useState<{ value: string; label: string }[]>([]);
+  const [filteredBookTitles, setFilteredBookTitles] = useState<{ value: string; label: string }[]>([]);
+  useEffect(() => {
+    const fetchBookTitles = async () => {
+      try {
+        const response = await fetch("http://127.0.0.1:5000/get_all_book_names");
+        if (!response.ok) {
+          throw new Error("Failed to fetch book titles");
+        }
+        const data = await response.json();
+        const mappedData = data
+        // .slice(0, 50) // Take only the first 50 entries
+        .map((s: string) => ({ value: s, label: s })); // Map strings to objects
+        setAllBookTitles(mappedData); // Assuming setAllBookTitles expects an array of objects
+        setFilteredBookTitles(mappedData)
+      } catch (error) {
+        console.error("Error fetching book titles:", error);
+      }
+    };
+  
+    fetchBookTitles();
+  }, []);
+  
+  useEffect(() => {
+    console.log("searchTitle changed")
+    if (searchTitle === "") {
+      setFilteredBookTitles(allBookTitles); // Reset to all titles if searchTitle is empty
+      return;
+    }
+  
+    const filtered = allBookTitles.filter((book) =>
+      book.label.toLowerCase().includes(searchTitle.toLowerCase())
+    );
+    setFilteredBookTitles(filtered);
+  }, [searchTitle, allBookTitles]); // Dependencies: searchTitle and allBookTitles
 
   async function handleSubmit(event: FormEvent<HTMLFormElement>) {
 
@@ -62,6 +100,14 @@ export default function RecommendBook() {
             placeholder="Search title"
             onChange={(e) => setSearchTitle(e.target.value)}
             className="w-1/3"
+          />
+          <Autocomplete
+            selectedValue={selectedTitle}
+            onSelectedValueChange={setSelectedTitle}
+            searchValue={searchTitle}
+            onSearchValueChange={setSearchTitle}
+            items={filteredBookTitles}
+            placeholder="Search for a book"
           />
           <Button type="submit">Search</Button>
         </form>
